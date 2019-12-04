543d58fe9655d6234de9d0cc85ef92bd61e0314e
Alden Shirtz <aldenshirtz@Aldens-MacBook-Air.local>
2019-12-04 12:38:59 -0600

    initial commit

---
+++ Practice/index.js
@@ -0,0 +1,8 @@
+const myButton = document.getElementById("alden-button");
+
+
+function aldenFunction(event) {
+    console.log("The button has been clicked!");
+}
+
+myButton.addEventListener('click', aldenFunction); 
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/mainPage.html'));
+});
+
+app.get('/practice', function(req, res) {
+    res.sendFile(path.join(__dirname + '/Practice/index.html'));
+})
+
+app.get('/fieldgoal', function(req, res) {
+    res.sendFile(path.join(__dirname + '/FieldGoal/fieldGoal.html'));
+})
+
+var server = app.listen(8080);
+console.log('Express server started on port %s', server.address().port);
